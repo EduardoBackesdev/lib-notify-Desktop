e9ddd70c4a8ba43a0f6ba96a2f0f18b1e0e3ead4
Eduardo Backes <128044560+EduardoBackesdev@users.noreply.github.com>
2024-11-13 22:11:51 -0300

    new lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod notify;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -8,7 +10,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        notify::simpleNotify::simpleNotify();
     }
 }
